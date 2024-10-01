013545c3e61fc8e490065983daf58f202336dde2
Agustin-dotcom <Agustin.Prieto2@alu.uclm.es>
2024-10-01 08:24:14 +0200

    added "whereto" attribute to "intersections"

--- googleMaps.py
+++ googleMaps.py
@@ -1,15 +1,25 @@
+import json
+import os
 class State:
     def __init__(self,current,acumulatedCost):
         self.current = current
         self.acumulatedCost = acumulatedCost
 class Node:
-    def __init__(self,parent,state):
+    def __init__(self,parent,state,action):
         self.parent = parent
         self.state = state # we want it to be type State
+        self.action = action # the same with action is going to be a class
 class Problem: # TODO
-    pass
+    def __init__(self,file_name):
+        with open(file_name,'r') as file:
+            new_dictionary = json.load(file)
+            for i in new_dictionary['intersections']:
+                i['whereto'] = [j['destination'] for j in new_dictionary['segments'] if j['origin'] == i['identifier']]
+                
 class Action: # Maybe we can receive a state and return a new one 
+    def __init__(self,action):
+        self.action = action
     pass # TODO
 def main():
-    print("Hello World!")
+    problem = Problem('paseo_simón_abril_albacete_250_1')
 main()
