3c29a4700095c92baeb2beca07e6087734df2f22
Agustin-dotcom <Agustin.Prieto2@alu.uclm.es>
2024-10-01 09:39:00 +0200

    cambie atributos de lugar como "current" y "accumulatedCost". Tambien añadi un .json, un problema. Me da un traceback se esta quejando sobre que mi __str__ esta devolviendo un none

--- googleMaps.py
+++ googleMaps.py
@@ -1,24 +1,35 @@
 import json
 import os
 class State:
-    def __init__(self,current,acumulatedCost):
-        self.current = current
-        self.acumulatedCost = acumulatedCost
+    def __init__(self,state):
+        self.state = state
 class Node:
     def __init__(self,parent,state,action):
         self.parent = parent
         self.state = state # we want it to be type State
         self.action = action # the same with action is going to be a class
-class Problem: # TODO
+    def __str__(self):
+        print("parent --> "+ str(self.parent))
+        print("state --> "+ str(self.state))
+        print("action --> "+ str(self.action))
+class Problem: # TODO # yo pondria "current" y "accumulatedCost" aqui en la clase Problem
     def __init__(self,file_name):
         with open(file_name,'r') as file:
             new_dictionary = json.load(file)
-            for i in new_dictionary['intersections']:
-                i['whereto'] = [{"id":j['destination'],"cost":j['distance']/j['speed']} for j in new_dictionary['segments'] if j['origin'] == i['identifier']]
+        for i in new_dictionary['intersections']:
+            i['whereto'] = [{"id":j['destination'],"cost":j['distance']/j['speed']} for j in new_dictionary['segments'] if j['origin'] == i['identifier']]
+        self.dictionary = new_dictionary    
+        # and here I guess we are ready to start the problem
+
+    def initializeOpen(self,initial):
+        self.root = Node(None,initial,None) # no estoy seguro si para llegar al nodo raiz action == None
 class Action: # Maybe we can receive a state and return a new one 
     def __init__(self,action):
         self.action = action
     pass # TODO
 def main():
-    problem = Problem('paseo_simón_abril_albacete_250_1')
+    #os.chdir("C:\googleMapsVS\Google-Maps")
+    problem = Problem("paseo_simón_abril_albacete_250_1.json")
+    problem.initializeOpen(problem.dictionary['initial'])
+    print(problem.root)
 main()
