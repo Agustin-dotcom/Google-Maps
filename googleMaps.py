b46b65fd2aa6936c06c5a90ce6b71b10c8a004c3
Agustin-dotcom <Agustin.Prieto2@alu.uclm.es>
2024-10-02 17:00:54 +0200

    he implementado los insert y extract de una manera sencilla. Hagamoslo lo más sencillo posible, ya habrá tiempo para complicarse

--- googleMaps.py
+++ googleMaps.py
@@ -1,5 +1,6 @@
 import json
 import os
+from collections import deque
 class State:
     def __init__(self,state):
         self.state = state
@@ -11,7 +12,7 @@ class Node:
     def __str__(self):
         print("parent --> "+ str(self.parent))
         print("state --> "+ str(self.state))
-        print("action --> "+ str(self.action))
+        print("action --> (origin, destination,cost) --> "+ str(self.action.origin)+", "+str(self.action.destination)+", "+str(self.action.cost))
 class Problem: # TODO # yo pondria "current" y "accumulatedCost" aqui en la clase Problem
     def __init__(self,file_name):
         with open(file_name,'r') as file:
@@ -22,13 +23,38 @@ class Problem: # TODO # yo pondria "current" y "accumulatedCost" aqui en la clas
         # and here I guess we are ready to start the problem
         self.initializeOpen(self.dictionary['initial']) # inicializo nodo raiz
     def initializeOpen(self,initial):
-        self.root = Node(None,initial,None) # no estoy seguro si para llegar al nodo raiz action == None
+        self.root = Node(None,initial,Action(None,initial,0)) # no estoy seguro si para llegar al nodo raiz action == None
 class Action: # Maybe we can receive a state and return a new one 
-    def __init__(self,action):
-        self.action = action
-    pass # TODO
+    def __init__(self,origin,destination,cost):
+        self.origin = origin
+        self.destination = destination
+        self.cost = cost
+class Search: # this is where we use inheritance
+    recoverPath = [] # yo lo pondría en problema
+    def insert():
+        pass
+    def extract():
+        pass
+class BreadthFirst(Search):# FIFO queue
+    def insert(self,succesor,open):
+        open.append(succesor)
+    def extract(self,open):
+        return open.popleft() # extrae por la izquierda (el primero en llegar)
+class DepthFirst(Search): # LIFO queue
+    def insert(self,sucessor,open):
+        open.append(sucessor)
+    def extract(self,open):
+        return open.pop() # lo del return tiene sentido yo creo
 def main():
     #os.chdir("C:\googleMapsVS\Google-Maps")
     problem = Problem("paseo_simón_abril_albacete_250_1.json")
-    print(problem.root)
+    #print(problem.root)
+    clase1 = BreadthFirst()
+    cola = deque()
+    cola.append('a')
+    cola.append('g')
+    #print(clase1.extract(cola))
+    print(cola)
+    print(clase1.extract(cola))
+    print(cola)
 main()
