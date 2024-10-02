ef878a356a1702dfcb6b106c78466cbd1d96f500
Agustin-dotcom <Agustin.Prieto2@alu.uclm.es>
2024-10-02 18:55:00 +0200

    me motivé un poco programando, hay que testear esto y juntarlo todo llamando a la función "search"

--- googleMaps.py
+++ googleMaps.py
@@ -22,11 +22,12 @@ class Problem: # TODO # yo pondria "current" y "accumulatedCost" aqui en la clas
             new_dictionary = json.load(file)
         for i in new_dictionary['intersections']:
             i['whereto'] = [{"id":j['destination'],"cost":j['distance']/j['speed']} for j in new_dictionary['segments'] if j['origin'] == i['identifier']]
-        self.dictionary = new_dictionary    
+        self.dictionary = new_dictionary   #!!!!!! 
         # and here I guess we are ready to start the problem
         self.initializeOpen(self.dictionary['initial']) # inicializo nodo raiz
     def initializeOpen(self,initial):
-        self.root = Node(None,initial,Action(None,initial,0),0) # no estoy seguro si para llegar al nodo raiz action == None
+        self.root = Node(None,State(initial),Action(None,initial,0),0) # no estoy seguro si para llegar al nodo raiz action == None
+        self.listOfActionsForRecoverPath.append(self.root.action)
     def testGoal(self,Node):
         return self.dictionary['final'] == Node.state
     def search(self,Search): # Search asumimos que es o BreadthFirst o DepthFirst
@@ -37,13 +38,44 @@ class Problem: # TODO # yo pondria "current" y "accumulatedCost" aqui en la clas
             node = Search.extract()
             if node.state.state not in explored:
                 if(self.testGoal(node.state.state)):
-                    return recoverPath(node)
-    def expand(self,Search):
-        open = []
-        thisSet = self.dictionary['intersections']['whereto'] # we get a dictionary of dictionaries
-        for each in thisSet:# we iterate through dictionaries with attributes {"id":x,"cost":y}
-            Search.insert(each["id"],open) # we insert into our open list the next nodes
-        return open
+                    return self.recoverPath(node)
+                if (node.depth < 1):
+                    successors = self.expand(node)
+                for  successor in successors:
+                    Search.insert(successor,successors)
+                explored.append(node.state.state) #  node.state es el objeto y node.state.state es la variable en el objeto state
+        return -1
+    def applyAction(self,State,Action):
+        """Given a state, we apply an action and return a new state.
+        Imagine we have a current state, for example, and then we apply an action.\
+        Importante decir que recibe objetos, no atributos
+
+        :return: the new state"""
+        if (Action.origin == State.state) :
+            self.listOfActionsForRecoverPath.append(Action)
+            return State(Action.destination)
+        print("No se puede aplicar la siguiente acción")
+        raise RuntimeError
+        
+
+    def recoverPath(self):
+        return self.listOfActionsForRecoverPath
+    def expand(self,Node_param):
+        """ 
+        :param Node_param: nodo al que apuntamos 
+        
+        :returns: list of """
+        successors = []
+        possibleActions = []
+        dictionaryOfDictionaries = self.dictionary['intersections']['identifier' == Node_param.state.state]['whereto'] # we get a dictionary of dictionaries
+        for dictionary in dictionaryOfDictionaries:# we iterate through dictionaries with attributes {"id":x,"cost":y}
+            possibleActions.append(Action(Node_param.state.state,dictionary["id"],dictionary["cost"])) # we insert into our open list the next nodes
+        for action in possibleActions:
+            newState = self.applyAction(Node_param.state,action) # Node.state es un objeto de tipo state
+            newNode = Node(Node_param,newState,action,Node_param.depth+1)
+            successors.append(newNode)
+        return successors
+
 
 class Action: # Maybe we can receive a state and return a new one 
     def __init__(self,origin,destination,cost):
@@ -78,4 +110,7 @@ def main():
     print(cola)
     print(clase1.extract(cola))
     print(cola)
+    agus = Node(None,State(12),Action(None,12,0),0)
+    print(agus.state.state)
+
 main()
