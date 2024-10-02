95e5178eb4acc742198702d5b8f6d9e23ccd4386
Agustin-dotcom <Agustin.Prieto2@alu.uclm.es>
2024-10-02 17:29:44 +0200

    acabo de añadir "expand" y "search" methods

--- googleMaps.py
+++ googleMaps.py
@@ -5,15 +5,18 @@ class State:
     def __init__(self,state):
         self.state = state
 class Node:
-    def __init__(self,parent,state,action):
+    def __init__(self,parent,state,action,depth):
         self.parent = parent
         self.state = state # we want it to be type State
         self.action = action # the same with action is going to be a class
+        self.depth = depth
     def __str__(self):
         print("parent --> "+ str(self.parent))
         print("state --> "+ str(self.state))
         print("action --> (origin, destination,cost) --> "+ str(self.action.origin)+", "+str(self.action.destination)+", "+str(self.action.cost))
+        print("depth --> "+ str(self.depth))
 class Problem: # TODO # yo pondria "current" y "accumulatedCost" aqui en la clase Problem
+    listOfActionsForRecoverPath = [] # lista a construir bottom-up
     def __init__(self,file_name):
         with open(file_name,'r') as file:
             new_dictionary = json.load(file)
@@ -23,7 +26,25 @@ class Problem: # TODO # yo pondria "current" y "accumulatedCost" aqui en la clas
         # and here I guess we are ready to start the problem
         self.initializeOpen(self.dictionary['initial']) # inicializo nodo raiz
     def initializeOpen(self,initial):
-        self.root = Node(None,initial,Action(None,initial,0)) # no estoy seguro si para llegar al nodo raiz action == None
+        self.root = Node(None,initial,Action(None,initial,0),0) # no estoy seguro si para llegar al nodo raiz action == None
+    def testGoal(self,Node):
+        return self.dictionary['final'] == Node.state
+    def search(self,Search): # Search asumimos que es o BreadthFirst o DepthFirst
+        open = Search.insert(self.root)
+        explored = []
+        while open is not None:
+            #Search.insert()
+            node = Search.extract()
+            if node.state.state not in explored:
+                if(self.testGoal(node.state.state)):
+                    return recoverPath(node)
+    def expand(self,Search):
+        open = []
+        thisSet = self.dictionary['intersections']['whereto'] # we get a dictionary of dictionaries
+        for each in thisSet:# we iterate through dictionaries with attributes {"id":x,"cost":y}
+            Search.insert(each["id"],open) # we insert into our open list the next nodes
+        return open
+
 class Action: # Maybe we can receive a state and return a new one 
     def __init__(self,origin,destination,cost):
         self.origin = origin
