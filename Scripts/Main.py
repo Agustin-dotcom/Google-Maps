9b75dff188de437fccd1b5a98a22948809214e25
Agustin-dotcom <Agustin.Prieto2@alu.uclm.es>
2024-10-13 17:27:13 +0200

    Added Database and problems

--- Scripts/Main.py
+++ Scripts/Main.py
@@ -3,6 +3,7 @@ from Problem import Problem
 from BreadthFirst import BreadthFirst
 from DepthFirst import DepthFirst
 from collections import deque
+
 def main():
     #os.chdir("C:\googleMapsVS\Google-Maps")
     #problem = Problem("paseo_simón_abril_albacete_250_1.json")
@@ -17,6 +18,26 @@ def main():
     #print(cola)
     #agus = Node(None,State(12),Action(None,12,0),0)
     #print(agus.state.state)
+    #os.curdir()
+    directorio = "C:\\googleMapsVS\\Google-Maps\\problems\\small"
+    archivos = os.listdir(directorio)
+    print(archivos)
+
+    for i in archivos:
+        os.chdir(directorio)
+        problem = Problem(i)
+        print(f"#################################################")
+        print(f"#            DEPTH FIRST SEARCH                 #")
+        print(f"#################################################")
+        print(f"#            {i}                 #")
+        print(f"#################################################")
+        print(';'.join(map(str,problem.search(DepthFirst()))))
+        print(f"#################################################")
+        print(f"#            BREADTH FIRST SEARCH                 #")
+        print(f"#################################################")
+        print(f"#            {i}                 #")
+        print(f"#################################################")
+        print(';'.join(map(str,problem.search(BreadthFirst()))))
     #problem.search(DepthFirst())
     #######
     #problem = Problem('paseo_simón_abril_albacete_250_1.json')
@@ -24,15 +45,14 @@ def main():
     #
     #problem = Problem('paseo_simón_abril_albacete_250_1.json')
     #print(';'.join(map(str,problem.search(DepthFirst()))))
-    directorio = 'C:\googleMaps\Google-Maps\Scripts\problemas'
-
-    # Obtiene la lista de archivos y carpetas en el directorio
-    archivos = os.listdir(directorio)
-
-    # Filtra solo los archivos (opcional)
-    solo_archivos = [f for f in archivos if os.path.isfile(os.path.join(directorio, f))]
-    #print(solo_archivos[0])
-    # Imprime la lista de archivos
-    problema = Problem(os.path.join(directorio,solo_archivos[0]))
-    print(';'.join(map(str,problema.search(DepthFirst()))))
+    # Obtiene el directorio actual donde está ejecutándose el script
+    # o pide al usuario que lo proporcione
+    
+    #C:\googleMapsVS\Google-Maps\Scripts\problemas\
+    #problema = Problem("calle_mariÌa_mariÌn_500_0.json")
+        
+    # Ejecuta la búsqueda y muestra los resultados
+    #print(';'.join(map(str, problema.search(DepthFirst()))))
+    #print(';'.join(map(str, problema.search(BreadthFirst()))))
+    
 main()
