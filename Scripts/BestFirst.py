665af1717b3227cef4bfdbf982c5081308b70513
Agustin-dotcom <Agustin.Prieto2@alu.uclm.es>
2024-10-14 07:12:09 +0200

    Añadí priorityQueue

--- Scripts/BestFirst.py
+++ Scripts/BestFirst.py
@@ -1,8 +1,11 @@
+import heapq # O(log n )
 from Search import Search
 class BestFirst(Search): # takes into account only h(n)
     def __init__(self):
-        pass
-    def extract():#We have to extract the one with the lowest f(n) = h(n)
-        pass
-    def insert():#We have to insert taking into account f(n) as well (we call it h which is the priority the heuristic)
-        pass
+        self.priorityQueue = []
+    def extract(self):#We have to extract the one with the lowest f(n) = h(n)
+        if len(self.priorityQueue)<1:
+            raise Exception(ErrorMessage = "No puedes extraer algo que no existe")
+        return heapq.heappop(self.priorityQueue)
+    def insert(self,element):#We have to insert taking into account f(n) as well (we call it h which is the priority the heuristic)
+        heapq.heappush(element,self.priorityQueue) # element is going to be a paired value (h,Node)
