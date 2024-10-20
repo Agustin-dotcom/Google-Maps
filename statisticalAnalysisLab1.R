cc5311ea250e03e829706a8185cd59148c73348a
Agustin-dotcom <Agustin.Prieto2@alu.uclm.es>
2024-10-20 22:50:53 +0200

    Ya funciona la BBDD e incluso he puesto algunas gráficas muy chulas. Me he pasado a RStudio para hace gráficas más bonitas porque en Python no controlo tanto

---
+++ statisticalAnalysisLab1.R
@@ -0,0 +1,54 @@
+rm(list=ls())
+source("C:/Users/Agus/OneDrive - Universidad de Castilla-La Mancha/Escritorio/2do año/2do cuatri/Statistics (Fernando)/R/Libraries/getMe.libraries.R")
+getMe.libraries()
+install.packages("RSQLite")
+library(RSQLite)
+
+# Especifica la ruta a tu archivo .db
+db_path <- "C:\\googleMapsVS\\Google-Maps\\resultados_programa.db"
+
+# Conéctate a la base de datos
+conn <- dbConnect(SQLite(), dbname = db_path)
+
+# Lista las tablas en la base de datos
+tables <- dbListTables(conn)
+print(tables)
+
+# Lee una tabla específica en un data frame
+df <- dbReadTable(conn, "resultados")
+print(head(df))
+
+# Cierra la conexión
+dbDisconnect(conn)
+DATA <- df
+nrow(DATA)
+summary(DATA)
+
+DATA$typeOfProblem <- factor(DATA$typeOfProblem)
+
+DATA$algorithm <- factor(DATA$algorithm)
+summary(DATA)
+
+library(ggplot2)
+ggplot(DATA, aes(y = executionTime, fill = algorithm))+
+  geom_boxplot(alpha = 0.7)+
+  ylim(-0.04,0.04)
+.desc.numeric(DATA$executionTime)$min
+DATA[DATA$executionTime <0,]
+
+
+summary(DATA)
+
+ggplot(DATA,aes(x = DATA$SolutionCost, fill = DATA$algorithm))+
+  geom_density(alpha = 0.7)+
+  xlim(0,2000)
+
+ggplot(DATA,aes(y=DATA$SolutionCost, fill = DATA$algorithm))+
+  geom_boxplot(alpha = 0.8)+
+  ylim(0,1000)
+
+ggplot(DATA,aes(x = typeOfProblem,y=SolutionCost,fill = algorithm))+
+  geom_bar(stat = "identity",position = "dodge")
+
+summary(DATA)
+.desc.numeric(DATA$executionTime,DATA$algorithm)
