d6708a1dc4e2728032178f9d06da7e76e376d836
Agustin-dotcom <Agustin.Prieto2@alu.uclm.es>
2024-12-10 18:43:52 +0100

    He cambiado de nuevo lo de antes

--- Lab2/Scripts/search.py
+++ Lab2/Scripts/search.py
@@ -29,7 +29,7 @@ class Search(ABC):
         # 1. Iterate through each station
         for idx,i in enumerate(self.problem.dictionary.get('candidates').values()): # O(n^2)
             min_of_all_a_star = float('inf') 
-            if solution[1][idx] == 0:
+            if solution[idx] == 0:
                 continue 
             pop = i.get('population')
             total_population += pop 
